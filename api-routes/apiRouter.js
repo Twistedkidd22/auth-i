9f500826cf66bbb80afffe4ee2d8a0fc2faa4b73
Jasper <joshtietz11@gmail.com>
2018-08-07 14:57:55 -0500

    added sessions, cookies, and logout functionality

--- api-routes/apiRouter.js
+++ api-routes/apiRouter.js
@@ -1,18 +1,37 @@
 const express = require('express')
+const session = require('express-session')
 const bcrypt = require('bcrypt')
 
 const db = require('../data/db.js');
 
 const router = express.Router();
 
+router.use(
+  session({
+    name: 'notdefaultanymore',
+    secret: 'i drink and i know things',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false
+  })
+)
+
 router.use(express.json());
 
 router.get('/users', (req, res) => {
   db('users')
   .then(response => {
+    if (req.session.username === 'codexx') {
     res.status(200).json(response);
+  } else{
+    res.status(401).send(`the user '${req.session.username}' is not authorized to view this page`)
+  }
   })
   .catch(error => {
+    console.log(error.message)
     res.status(404).send('error fetching users...')
   })
 })
@@ -48,18 +67,22 @@ router.post('/register', (req, res) => {
       if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
         return res.status(401).json({ error: 'invalid username or password' });
       } else {
-        return res.status(200).send("...I'm in...")
+        req.session.username = user.username
+        return res.status(200).send(`welcome ${user.username}`)
       }
     })
+  })
 
-    // db('users')
-    // .insert(user)
-    // .then(ids => ({ id: ids[0] }),
-    // res.status(200).json(user))
-    //   .catch(error => {
-    //     res.status(500).send('error adding user...')
-    //     console.log(error.message)
-    //   })
-    })
+  router.get('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send('error logging out')
+        } else {
+          res.send('goodbye...')
+        }
+      })
+    }
+  })
 
 module.exports = router;
